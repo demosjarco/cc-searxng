4596ebd1d75f8e7b607134cc13a439c3c9c0de31
Victor <1209494+demosjarco@users.noreply.github.com>
2025-05-07 22:11:00 -0700

    Not needed

--- src/index.ts
+++ src/index.ts
@@ -68,7 +68,6 @@ export default class extends WorkerEntrypoint<EnvVars> {
 
 		// Debug
 		app.use('*', (c, next) => import('hono/timing').then(({ timing }) => timing()(c, next)));
-		app.use('*', (c, next) => Promise.all([import('hono/combine'), import('hono/logger')]).then(([{ except }, { logger }]) => except(() => c.env.NODE_ENV !== 'development', logger())(c, next)));
 
 		app.all('*', (c) =>
 			Promise.all([
